15c299326c135a235f5b0fd2dd0eb8c0bf4dacf3
Gaurav Haldar <haldargaurav04@gmail.com>
2024-09-27 21:34:32 +0530

    Data Collection

--- dataCollection.py
+++ dataCollection.py
@@ -1,11 +1,17 @@
 from cvzone.FaceDetectionModule import FaceDetector
 import cv2
 import cvzone
-
+from time import time
+############################
+imageOutputPath = "Datasets/Data Collect"
 offsetWratio = 10
 offsetHratio = 20
 confidence = 80
-
+debug = False
+save = True
+floatingpoint = 6
+blurThreshold = 35
+#############################
 camWidth,camHeight = 640,480
     # Initialize the webcam
     # '2' means the third camera connected to the computer, usually 0 refers to the built-in webcam
@@ -29,10 +35,13 @@ while True:
         # img: Updated image
         # bboxs: List of bounding boxes around detected faces
     img, bboxs = detector.findFaces(img, draw=False)
+    imgOUT = img.copy()
 
         # Check if any face is detected
     if bboxs:
             # Loop through each bounding box
+        listBlur = [] # Adding value of faces true if detected and false if not
+        listInfo = [] # Contains normalised values and classnames
         for bbox in bboxs:
                 # bbox contains 'id', 'bbox', 'score', 'center'
 
@@ -40,6 +49,7 @@ while True:
             center = bbox["center"]
             x, y, w, h = bbox['bbox']
             score = int(bbox['score'][0] * 100)
+            
 
             # ---- Checking Confidence ---- #
 
@@ -69,16 +79,22 @@ while True:
                 imgFace = img[y:y+h,x:x+w]
                 blurValue = int(cv2.Laplacian(imgFace,cv2.CV_64F).var())
 
+                if blurValue > blurThreshold:
+                    listBlur.append(True)
+                else:
+                    listBlur.append(False)
+
+
                 # ---- Normalization ---- #
                 iw , ih,_ = img.shape
 
                 xc = x + w/2
                 yc = y + h/2
 
-                xcn = round(xc/iw,6)
-                ycn = round(yc/ih,6)
-                wn = round(w/iw,6)
-                hn = round(h/ih,6)
+                xcn = round(xc/iw,floatingpoint)
+                ycn = round(yc/ih,floatingpoint)
+                wn = round(w/iw,floatingpoint)
+                hn = round(h/ih,floatingpoint)
                 
 
                 # ---- Avoiding values above 1 ---- #
@@ -87,20 +103,37 @@ while True:
                 if ycn > 1 : ycn = 1
                 if wn > 1 : wn = 1
                 if hn > 1 : hn = 1
-                print(xcn,ycn,wn,hn)
+                # print(xcn,ycn,wn,hn)
 
 
                 # ---- Drawing ---- #
-
+                
                 cv2.imshow("Face",imgFace)
-                cv2.rectangle(img,(x ,y, w, h),(255,0,0),3)
-                cvzone.putTextRect(img,f"Score:{score}% Blur:{blurValue}",(x,y+20),scale=2,thickness=3)
+                cv2.rectangle(imgOUT,(x ,y, w, h),(255,0,0),3)
+                cvzone.putTextRect(imgOUT,f"Score:{score}% Blur:{blurValue}",(x,y+20),scale=2,thickness=3)
+                if debug == True:
+                    cv2.imshow("Face",imgFace)
+                    cv2.rectangle(img,(x ,y, w, h),(255,0,0),3)
+                    cvzone.putTextRect(img,f"Score:{score}% Blur:{blurValue}",(x,y+20),scale=2,thickness=3)
 
 
+        if save == True:
+
+            if all(listBlur) and listBlur != []:
+
+                timeNow = time()
+                timeNow = str(timeNow).split(".")
+                timeNow = int(timeNow[0]+timeNow[1])
+
+                cv2.imwrite(f"{imageOutputPath}/{timeNow}.jpg",img)
+
+                print(timeNow)
+            
+    
 
 
 
         # Display the image in a window named 'Image'
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", imgOUT)
         # Wait for 1 millisecond, and keep the window open
     cv2.waitKey(1)
